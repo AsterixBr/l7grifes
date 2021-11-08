cb6e2163f4f7ef535cfc0302aaf8f5ee7823bcee
AsterixBR <xandsbarbosa@gmail.com>
2021-11-08 11:43:10 -0300

    flip

--- Carrinho.php
+++ Carrinho.php
@@ -1,119 +1,174 @@
 <?php
-// ----------------------------------------------------------------	session_start();
-	require_once 'head.html';
-	include_once '../App/Controller/ProdutoController.php';
-	include_once '../App/Controller/CarrinhoController.php';
-	include_once '../App/Controller/ClienteController.php';
-	
-	$user = new ClienteController();
-	//$result = $user->isLoggedIn();	
+       
+	   if(isset($parametros[1]) && $parametros[1] == 'add' && isset($parametros[2]) && $parametros[2] != '0'){
+	         $idProd = (int)$parametros[2];
+	         $carrinho->verificaAdiciona($idProd);
+	     }
+
+	     if(isset($_SESSION['media_produto'][0])){unset($_SESSION['media_produto'][0]);}
+         if(count($_SESSION['media_produto']) == 0){unset($_SESSION['valor_frete']);}
+
+         if(isset($parametros[1]) && $parametros[1] == 'add' || isset($_POST['atualizar'])){
+		  /*  unset($_SESSION['valor_frete']);*/
+		    foreach($_SESSION['media_produto'] as $id => $qtd){
+		        unset($_SESSION['valor_frete_'.$id]);
+		    }
+		}
+	     
+	     if(isset($parametros[1]) && $parametros[1] == 'del' && isset($parametros[2])){
+         $idDel = (int)$parametros[2];
+    if($carrinho->deletarProduto($idDel)){
+         echo '<script>alert("Produto deletado com suscesso");location.href="'.PATH.'/carrinho"</script>';
+    }else{
+        echo '<script>alert("Erro ao deletar produto");location.href="'.PATH.'/carrinho"</script>';
+    }
+}
+
+    if(isset($_POST['prodSingle'])){
+	   $produtoValor = $_POST['prodSingle'];
+
+	   if($carrinho->setarByPost($produtoValor)){}else{
+	      echo '<p id="aviso">Não foi possível adicionar este produto ao carrinho de compras</p>';
+	   }
+	}
+
+    if(isset($_POST['atualizar'])){
+    $produto = $_POST['prod'];
+    foreach($produto as $chave => $qtd){
+    $selecionar_produto = BD::conn()->prepare("SELECT * FROM `loja_produtos` Where id = ?");
+    $selecionar_produto->execute(array($chave));  
+    $fetchProd = $selecionar_produto->fetchObject();
+    if($qtd > $fetchProd->estoque){
+         echo '<p id="aviso">Não é possível setar mais que: '.$fetchProd->estoque.' produtos para compra deste produto: '.$fetchProd->titulo.'</p>';
+         $warn = true;
+    }
+}
+
+	if($warn == true){}else{
+	    if($carrinho->atualizarQuantidades($produto)){
+	        echo '<script>alert("Quantidade foi alterada com sucesso");location.href="'.PATH.'/carrinho"</script>';
+	    }else{
+	        echo '<script>alert("Erro ao atualizar quantidade");location.href="'.PATH.'/carrinho"</script>';
+	    }
+	  }
+}
+ 
+
+   if(isset($_POST['acao']) && $_POST['acao'] == 'calcular'):
+	$frete = $_POST['frete'];
+    $_SESSION['frete_type'] = $frete;
+	$cep = strip_tags(filter_input(INPUT_POST, 'cep'));
+	switch($frete){
+	    
+	    case 'pac';
+	       $valor = '41106';
+	       $peso_total = 0;
+	       foreach($_SESSION['media_produto'] as $id => $qtd){ 
+	            $selecionar_produto = BD::conn()->prepare("SELECT peso FROM `loja_produtos` WHERE id = ?");
+	            $selecionar_produto->execute(array($id));
+	            $fetch_produto = $selecionar_produto->fetchObject();
+	            
+	            $_SESSION['valor_frete_'.$id] = $carrinho->calculaFrete($valor, 81750040, $cep, $fetch_produto->peso);
+	       }
+	    break;
+
+	    case 'sedex';
+	       $valor = '40010';
+	       $peso_total = 0;
+	       foreach($_SESSION['media_produto'] as $id => $qtd){ 
+	            $selecionar_produto = BD::conn()->prepare("SELECT peso FROM `loja_produtos` WHERE id = ?");
+	            $selecionar_produto->execute(array($id));
+	            $fetch_produto = $selecionar_produto->fetchObject();
+	            $_SESSION['valor_frete_'.$id] = $carrinho->calculaFrete($valor, 81750040, $cep, $fetch_produto->peso);
+	       }
+	     break;
+	}
+	endif;
+
+	$_SESSION['valor_frete'] = 0;
+	foreach($_SESSION['media_produto'] as $id => $qtd){
+	   $_SESSION['valor_frete_'.$id] = str_replace(",",".", $_SESSION['valor_frete_'.$id]);
+	   
+
+	   $_SESSION['valor_frete'] += $_SESSION['valor_frete_'.$id]*$qtd;
+    }
+
+
 ?>
-
-<!DOCTYPE HTML>
-<html>
-	
-	<body>
-		
-	<div class="colorlib-loader"></div>
-
-	<div id="page">
-		<nav class="colorlib-nav" role="navigation">
-			<div class="top-menu">
-				<div class="container">
-					<div class="row">
-						<div class="col-xs-2">
-							<div id="colorlib-logo"><a href="index.php">Use New Mic</a></div>
-						</div>
-						<div class="col-xs-10 text-right menu-1">
-							<ul>
-								<li><a href="index.php">Home</a></li>
-								<li class="active"><a href="shop.php">Produtos</a></li>
-								<?php
-									if ($result == true) {
-										echo '<li><a href="list.php"> Seus Produtos </a></li>';
-
-										echo '
-											<li><a href="cart.php"><i class="icon-shopping-cart"></i> Carrinho </a></li>
-											<li><a href="../App/Controller/logout.php"> Sair </a></li>';
-									}else{
-										echo '<li><a href="login.php"> Login/Cadastre-se </a></li>';
-									}
-								?>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</nav>
-		<aside id="colorlib-hero" class="breadcrumbs">
-			<div class="flexslider">
-				<ul class="slides">
-			   	<li style="background-image: url(images/4.jpg);">
-			   		<div class="overlay"></div>
-			   		<div class="container-fluid">
-			   			<div class="row">
-				   			<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
-				   				<div class="slider-text-inner text-center">
-				   					<h1>Produtos</h1>
-				   					<h2 class="bread"><span><a href="index.php">Home</a></span> <span>Produtos</span></h2>
-				   				</div>
-				   			</div>
-				   		</div>
-			   		</div>
-			   	</li>
-			  	</ul>
-		  	</div>
-		</aside>
-
-		<div class="colorlib-shop">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-8 col-md-push-2">
-						<div class="row row-pb-lg">
-							<?php
-								$produtos = ProdutoController::allProdutos();
-								foreach ($produtos as $produto) {
-									echo
-								'<div class="col-md-4 text-center">
-									<div class="product-entry">
-										<div class="product-img" style="background-image: url(images/'.$produto[4].'.jpg'.');"
-											<p class="tag"><span class="new"></span></p>
-											<div class="cart">
-												<p>
-													<span class="addtocart"><a href="../App/Controller/addCarrinho.php?produto='.$produto[0].'"><i class="icon-shopping-cart"></i></a></span> 
-												</p>
-											</div>
-										</div>
-										<div class="desc">
-											<h3>'.$produto[1].'</h3>
-											<p class="price"><span>R$ '.number_format($produto[3],2,",",".").'</span></p>
-										</div>
-									</div>
-								</div>';	
-								}
-							?>	
-							<div class="row">
-								<div class="col-md-12">
-									<ul class="pagination">
-										<li class="disabled"><a href="#">&laquo;</a></li>
-										<li class="active"><a href="#">1</a></li>
-										<li><a href="#">&raquo;</a></li>
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<?php
-					//include_once("footer.html");
-				?>
-			</div>
-
-		</div>
-	<div class="gototop js-top">
-		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
-	</div>
-	
-	</body>
-</html>
+<div id="carrinho-page">
+<h1 class="title-page"><img src="<?php echo PATH;?>/images/loja_cart_destaque.png" border="0" alt="" />Minhas Compras</h1>
+<form action="<?php echo PATH.'/carrinho/atualizar';?>" method="post" enctype="multipart/form-data">
+   <table border="1" cellpadding="0" class="carrinho">
+      <thead>
+          <tr>
+              <th>Produto</th>    
+              <th>Quantidade</th>
+              <th>Valor Unitário</th>
+              <th>Sub Total</th>
+              <th>Remover Produto</th>
+          </tr>
+      </thead>
+      <tbody>
+          <?php
+	       if($carrinho->qtdProdutos() == 0){     
+	          echo '<tr><td colspan="5">Não existem produtos em seu carrinho</td></tr>';
+	       }else{
+	          $total = 0;
+		 foreach($_SESSION['media_produto'] as $id => $quantidade){ 
+		 	
+		    $id = (int)$id;
+		    $selecao = BD::conn()->prepare("SELECT * FROM `loja_produtos` WHERE id = ?");
+		    $selecao->execute(array($id));
+            $fetchProduto = $selecao->fetchobject();
+	  ?>
+           <tr>
+              <td><img src="<?php echo PATH;?>/produtos/<?php echo $fetchProduto->img_padrao;?>" width="100" title="<?php echo $fetchProduto->titulo;?>" id="prodimg" alt="" border="0" /><span><?php echo $fetchProduto->titulo;?></span></td>
+	      <td><input type="text" name="prod[<?php echo $id;?>]" value="<?php echo $quantidade;?>" size="3" /></td>
+	      <td class="unitario">R$ <?php echo number_format($fetchProduto->valor_atual, 2,',','.');?></td>
+	      <td class="sub">R$ <?php echo number_format($fetchProduto->valor_atual * $quantidade, 2,',','.');?></td>
+	      <td><a href="<?php echo PATH.'/carrinho/del/'.$id;?>" title="Deletar Produto"><img src="<?php echo PATH;?>/images/del.png" border="0" alt="" /></a></td>
+           </tr> 
+           <?php $total += $fetchProduto->valor_atual *$quantidade;}}?>
+           <tr>  
+	    <td colspan="4" align="right" class="last">Total</td>
+	    <td class="total, last">R$ <?php echo (isset($_SESSION['valor_frete'])) ?  number_format($total+$_SESSION['valor_frete'],2,',','.') : number_format($total,2,',','.');?></td>
+	   </tr>
+     </tbody>
+     </table>
+     <input type="submit" value="Atualizar Quantidade" id="update" name="atualizar" />
+</form> 
+<div id="opcoes">
+    <div id="outros">
+	   <span id="resultado-frete">Valor do frete: 
+	   <?php echo $_SESSION['valor_frete'];?>
+	   </span>
+	   <a href="<?php echo PATH.'/verificar';?>" id="finalizar">Finalizar Compra</a>
+	   <a href="<?php echo PATH;?>" id="continuar">Continuar Comprando</a>
+     </div><!-- outros -->
+   <div class="calcular">
+       <form action="<?php echo PATH.'/carrinho';?>" method="post" enctype="multipart/form-data">
+          <input type="submit" value="Calcular Frete" />
+	    <label>
+	         <span>Escolha a forma de envio</span>
+	         <select name="frete">
+	               <option value="">Selecione</option>
+	               <option value="pac">PAC</option>
+	               <option value="sedex">SEDEX</option>
+	         </select>
+	    </label>
+	    <label>
+	         <span>Seu CEP</span>
+	         <input type="text" name="cep" />
+	    </label>
+	    <input type="hidden" name="acao" value="calcular" />
+	    
+	</form>
+   </div><!-- calcular -->
+</div><!-- opçoes -->
+</div><!-- carrinho page -->
+<?php (isset($_SESSION['valor_frete'])) ? 
+	   $_SESSION['total_compra'] = number_format($total+$_SESSION['valor_frete'],2,',','.'):
+	   $_SESSION['total_compra'] = number_format($total,2,',','.');
+           $_SESSION['total_compra'] = str_replace(",",".", $_SESSION['total_compra']);
+           
+?>
